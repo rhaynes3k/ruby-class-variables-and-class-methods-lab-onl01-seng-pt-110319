acfaeb004fded1e3ae45c740f4188f53d4da2d5f
Rommel Haynes <rhaynes3k@gmail.com>
2019-12-06 00:54:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,10 +42,11 @@ class Song
     @@genres.each_with_index do |g, i|
       # {}
       # if there is not a key in the hash -> make the value 1
+      num = 1
       if !@@g_count.has_key(g)
-        @@g_count[g][[1] += 1]
+        @@g_count[g] = num
       # if the key is already there we need to INCREMENT the value
-      else @@g_count[g] = 1 += 1
+      else @@g_count[g] = num += 1
       end 
     end
     @@g_count
